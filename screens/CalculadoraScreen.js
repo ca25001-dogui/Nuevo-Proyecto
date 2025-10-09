e62fa802c2baf41fa4bc06d85c883b46497f5efd
Kevin Canales <kevscanales@outlook.com>
2025-10-08 20:20:44 -0600

    Esta empezado un nuevo proyecto

---
+++ screens/CalculadoraScreen.js
@@ -0,0 +1,11 @@
+//Dependencias 
+import {View, Text, TextInput, Button} from 'react-native';
+
+export default function CalculadoraScreen(){
+     return (
+        <View>
+            <Text>Calculadora</Text>
+            
+        </View>
+     );
+}
