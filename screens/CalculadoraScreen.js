3be0a1c4263755f98cf1e46ddf4458190e8a7af9
Kevin Canales <kevscanales@outlook.com>
2025-10-09 20:48:39 -0600

    Avance de la tarea de la APP

--- screens/CalculadoraScreen.js
+++ screens/CalculadoraScreen.js
@@ -1,11 +1,25 @@
 //Dependencias 
 import {View, Text, TextInput, Button} from 'react-native';
+import { StyleSheet } from 'react-native/types_generated/index';
 
 export default function CalculadoraScreen(){
      return (
         <View>
             <Text>Calculadora</Text>
-            
-        </View>
+            <TextInput 
+             keyboardType="numeric"
+             placeholder="Primer numero" />
+            <TextInput 
+             keyboardType="numeric"
+             placeholder="Segundo numero" />
+            <Button
+             color="#555" 
+             title="Calcular" />
+               <Text>Resultado:</Text>
+            </View>
      );
 }
+
+const styles = StyleSheet.create({
+
+});
