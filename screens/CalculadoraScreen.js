ebd316637762791268ce81829035c4bfc9e6f8fb
Kevin Canales <kevscanales@outlook.com>
2025-10-16 16:49:36 -0600

    Ya hice las notas con su promedio

--- screens/CalculadoraScreen.js
+++ screens/CalculadoraScreen.js
@@ -6,36 +6,60 @@ export default function CalculadoraScreen(){
 
   const [num1, setNum1] = useState('');
   const [num2, setNum2] = useState('');
+  const [num3, setNum3] = useState('');
   const [resultado, setResultado] = useState(null);
+  const [estado, setEstado] = useState(null);
 
   function Calcular(){
   //Convertir a numero 
   const n1 = parseFloat(num1) || 0;
   const n2 = parseFloat(num2) || 0;
-  setResultado(n1 + n2);
-}
+  const n3 = parseFloat(num3) || 0;
 
+  const promedio = (n1 + n2 + n3) / 3;
+  const redondeado = parseFloat(promedio.toFixed(1)); // redondea a 1 decimal
+
+  setResultado(redondeado);
+  console.log("Porcentaje:", redondeado);
+
+  if (redondeado >= 7.5) {
+      setEstado('Aprobado');
+    } else {
+      setEstado('Reprobado');
+    }
+  }
 
 
      return (
         <View style={styles.container}> 
-            <Text>Calculadora</Text>
+            <Text>Nota 1</Text>
             <TextInput 
             value={num1}
             onChangeText={setNum1}
             style={styles.input}
              keyboardType="numeric"
-             placeholder="Primer Numero" />
+             placeholder="Primera Nota" />
+             <Text>Nota 2</Text>
             <TextInput 
-             value={num2}
+            value={num2}
             onChangeText={setNum2}
             style={styles.input}
              keyboardType="numeric"
-             placeholder="Segundo numero" />
+             placeholder="Segunda Nota" />
+             <Text>Nota 3</Text>           
+            <TextInput 
+            value={num3}
+            onChangeText={setNum3}
+            style={styles.input}
+             keyboardType="numeric"
+             placeholder="Tercera Nota" />
             <Button onPress={Calcular}
              color="#555" 
              title="Calcular" />
                <Text style={styles.text}> Resultado:{resultado}</Text>
+               <p></p>
+               <Text style={styles.text}> Estado:{estado}</Text>
+               
             </View>
      );
 }
